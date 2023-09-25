62ad107ae28a3419876c1269406e104ad0a0951c
zakariaar27 <mdzakaria8421@gmail.com>
2023-09-26 04:30:14 +0600

    upgrade

--- lib/const/image_path.dart
+++ lib/const/image_path.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class KImage {
   static const main = "assets/img.jpg";
   static const signInImg = "assets/img1.png";
@@ -7,4 +9,24 @@ class KImage {
   static const slider3 = "assets/imgs3.jpg";
   static const slider4 = "assets/imgs4.jpg";
   static const slider5 = "assets/imgs5.jpg";
+}
+
+// for first slider
+
+class KSliderImg extends StatelessWidget {
+  const KSliderImg({super.key, required this.assetImage});
+  final String assetImage;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 130,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(assetImage),
+        ),
+      ),
+    );
+  }
 }
--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:prac_five/const/image_path.dart';
 import 'package:prac_five/utiliz/height_weight.dart';
 import 'package:prac_five/widget/text.dart';
 import 'package:prac_five/widget/textfromfield.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import '../../const/image_path.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List sliderItem = [
+    Stack(
+      children: [
+        KSliderImg(
+          assetImage: KImage.slider1,
+        ),
+        KText(
+          signInData: 'Skilled Computer & IT',
+          color: Colors.white,
+          fontweight: FontWeight.bold,
+        ),
+        Positioned(
+          top: 70,
+          left: 20,
+          child: ElevatedButton(
+              onPressed: () {}, child: KText(signInData: "Admission Here")),
+        ),
+      ],
+    ),
+    Stack(
+      children: [
+        KSliderImg(
+          assetImage: KImage.slider2,
+        ),
+      ],
+    ),
+    Stack(
+      children: [
+        KSliderImg(
+          assetImage: KImage.slider3,
+        ),
+      ],
+    ),
+    Stack(
+      children: [
+        KSliderImg(
+          assetImage: KImage.slider4,
+        ),
+      ],
+    ),
+    Stack(
+      children: [
+        KSliderImg(
+          assetImage: KImage.slider5,
+        ),
+      ],
+    )
+  ];
+
+  int myIndex = 0;
+
+  getScale(int index) {
+    if (myIndex == index) {
+      return 1.0;
+    } else {
+      return 8.0;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +79,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: Color.fromRGBO(206, 206, 206, 1),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
               height: KAppSize.getHeight(context: context, size: 1),
@@ -54,20 +120,49 @@ class HomePage extends StatelessWidget {
               height: KAppSize.getHeight(context: context, size: 3),
             ),
             SizedBox(
-                height: KAppSize.getHeight(context: context, size: 150),
+                height: KAppSize.getHeight(context: context, size: 20),
                 child: PageView.builder(
-                  itemCount: 5,
+                  onPageChanged: (value) {
+                    setState(() {
+                      myIndex = value;
+                    });
+                  },
+                  controller:
+                      PageController(initialPage: 0, viewportFraction: 0.8),
+                  itemCount: sliderItem.length,
                   itemBuilder: (context, index) {
-                    return Stack(children: [
-                      Image.asset(KImage.slider1),
-                      Image.asset(KImage.slider2),
-                      Image.asset(KImage.slider3),
-                      Image.asset(KImage.slider4),
-                      Image.asset(KImage.slider5),
-                      KText(signInData: "$index")
-                    ]);
+                    double scale = getScale(index);
+                    return TweenAnimationBuilder(
+                      tween: Tween(begin: scale, end: scale),
+                      duration: Duration(seconds: 1),
+                      builder: (context, value, child) {
+                        return Transform.scale(
+                          scale: value,
+                        );
+                      },
+                      child: sliderItem[index],
+                    );
                   },
                 )),
+            KText(
+              signInData: 'Products',
+              color: Colors.blue,
+              fontSize: 25,
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    child: KText(signInData: '$index pro'),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
